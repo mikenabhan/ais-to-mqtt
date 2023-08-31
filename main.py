5227e713e3c8942b71e60830a592a3b44c847087
Mike Nabhan <13139061+mikenabhan@users.noreply.github.com>
2023-08-30 22:48:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ api_key = ''
 
 # MQTT configuration
 mqtt_broker_address = ""
-mqtt_topic = "ais_data"
+mqtt_base_topic = "ais_data"
 mqtt_username = ""
 mqtt_password = ""
 
 async def connect_ais_stream():
     async with websockets.connect("wss://stream.aisstream.io/v0/stream") as websocket:
-        subscribe_message = {"APIKey": api_key, "BoundingBoxes": [[[-11, -178], [30, 74]]], "FiltersShipMMSI": ["367590270"], "FilterMessageTypes": ["PositionReport"]}
+        subscribe_message = {"APIKey": api_key, "BoundingBoxes": [[[-11, -178], [30, 74]]], "FiltersShipMMSI": ["",""], "FilterMessageTypes": ["PositionReport"]}
 
         subscribe_message_json = json.dumps(subscribe_message)
         await websocket.send(subscribe_message_json)
@@ -28,17 +28,23 @@ async def connect_ais_stream():
         async for message_json in websocket:
             message = json.loads(message_json)
             ais_message = message['Message']['PositionReport']
+            ship_id = ais_message['UserID']
 
             # Create AIS message string
             ais_info = {
                 "timestamp": datetime.now(timezone.utc).isoformat(),
                 "shipId": ais_message['UserID'],
                 "latitude": ais_message['Latitude'],
-                "longitude": ais_message['Longitude']
+                "longitude": ais_message['Longitude'],
+                "trueHeading": ais_message['TrueHeading'],
+                "speedOverGround": ais_message['Sog'],
+                 "courseOverGround": ais_message['Cog'],
+                  "navigationalStatus": ais_message['NavigationalStatus']
             }
 
             # Publish AIS message to MQTT broker
             print(ais_info)
+            mqtt_topic = f"{mqtt_base_topic}/{ship_id}"
             mqtt_client.publish(mqtt_topic, payload=str(ais_info), qos=1)
 
 if __name__ == "__main__":
